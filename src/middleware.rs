36b6ca100c0d99045843239ee899e0c175178e16
Will Bird <scoper@gmail.com>
2026-05-08 00:05:09 +1000

    fix(csp): add strict-dynamic for YouTube nonce propagation, drop crossorigin attr

--- src/middleware.rs
+++ src/middleware.rs
@@ -12,7 +12,7 @@ const CSP_TEMPLATE: &str = "default-src 'self'; \
 img-src 'self' https://static-cdn.jtvnw.net https://i.ytimg.com https://cdn.7tv.app https://cdn.betterttv.net https://cdn.frankerfacez.com; \
 style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; \
 font-src 'self' https://fonts.gstatic.com; \
-script-src 'self' 'nonce-{NONCE}' https://unpkg.com https://www.youtube.com; \
+script-src 'self' 'nonce-{NONCE}' 'strict-dynamic' https://unpkg.com https://www.youtube.com; \
 connect-src 'self' https://7tv.io https://api.betterttv.net https://api.frankerfacez.com; \
 frame-src https://www.youtube.com https://player.twitch.tv; \
 frame-ancestors 'none'";
